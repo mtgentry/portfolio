10e76f50973fe81b4b7d39f73a49d14bce6e5b21
Viacheslav Khromiak <slava.khromyak@gmail.com>
2023-03-26 13:55:06 +0300

    add video

--- components/project/Section.vue
+++ components/project/Section.vue
@@ -1,12 +1,16 @@
 <!-- Please remove this file from your project -->
 <template lang="pug">
-  div.section(v-if="section.title")
+  div.section(v-if="section.title" data-aos="fade-up")
     span.font-weight-bold(v-if="section.title") {{ section.title }}
     p(v-for="text in section.text") {{ text }}
-  v-row.section.images(v-else)
+  v-row.section.images(v-else-if="section.images")
     v-col(v-for="image in section.images" :cols="12 / section.images.length" :key="image.name")
-      img(:src="`/projects/${$route.params.project}/media/${image.name}`")
-
+      img(:src="`/projects/${$route.params.project}/media/${image.name}`"  data-aos="fade-up")
+  v-row.section(v-else)
+    v-col(v-for="video in section.videos" :cols="12 / section.videos.length" :key="video.name")
+      video(autoplay muted loop data-aos="fade-up")
+        source(:src="`/projects/${$route.params.project}/media/${video.name}`" type="video/mp4")
+        span Your browser does not support the video tag.
 
 </template>
 
@@ -28,7 +32,6 @@ export default {
   justify-content: center
 
   img
-    min-height: 500px
     width: 100%
 
 
