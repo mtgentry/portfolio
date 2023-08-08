36ad9c3a86c87f56f6c7cdd5ab7a6b1db0d431f2
Viacheslav Khromiak <slava.khromyak@gmail.com>
2023-08-09 01:34:11 +0300

    temporary agency change

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,9 @@ import Homepage from "@/components/pages/Homepage.vue"
 import ExtraPage from "@/components/mixins/ExtraPage.vue"
 export default {
 
-  extends: process.env.IS_AGENCY ? ExtraPage : Homepage,
+  // extends: process.env.IS_AGENCY ? ExtraPage : Homepage,
+  extends: Homepage,
+
   data() {
     return {
       page_name: 'unlimited_design',
