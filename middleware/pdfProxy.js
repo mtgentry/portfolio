8bd4f76504d4ce50748a526e9a753e2e8a7d6daa
“Mason <mgentry@masongentry.com>
2023-09-06 10:14:42 -0700

    added code to debug middleware

--- middleware/pdfProxy.js
+++ middleware/pdfProxy.js
@@ -1,21 +1,38 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 export default function (req, res, next) {
-  // Check if the request URL matches the pattern for PDFs
-  if (/^\/.*\.pdf$/.test(req.url)) {
-    // Create a proxy middleware
-    const proxy = createProxyMiddleware({
-      target: 'http://localhost:3000',  // Your server URL
-      pathRewrite: {
-        '^/': '/domains/agency/pages/pitches/',  // Rewrite the URL path
-      },
-      changeOrigin: true,
-    });
+  try {
+    console.log(`Incoming request: ${req.method} ${req.url}`);  // Log the incoming request
 
-    // Use the proxy to forward the request
-    return proxy(req, res, next);
+    // Check if the request URL matches the pattern for PDFs
+    if (/^\/.*\.pdf$/.test(req.url)) {
+      console.log('PDF pattern matched, setting up proxy');  // Debug statement
+
+      // Create a proxy middleware
+      const proxy = createProxyMiddleware({
+        target: 'http://localhost:3000',  // Your server URL
+        pathRewrite: {
+          '^/': '/domains/agency/pages/pitches/',  // Rewrite the URL path
+        },
+        changeOrigin: true,
+        onError(err, req, res) {
+          console.error('Proxy error:', err);  // Log any proxy errors
+          res.status(500).send('An error occurred while proxying the request.');
+        },
+      });
+
+      console.log('Proxying request');  // Debug statement
+
+      // Use the proxy to forward the request
+      return proxy(req, res, next);
+    }
+
+    console.log('No PDF pattern matched, moving to next middleware');  // Debug statement
+
+    // Continue with the next middleware for other routes
+    next();
+  } catch (error) {
+    console.error('An error occurred in the middleware:', error);  // Log any unexpected errors
+    res.status(500).send('An unexpected error occurred.');
   }
-
-  // Continue with the next middleware for other routes
-  next();
 }
