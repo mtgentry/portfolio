4d7203335191eb6d37160eaa5367f740dcd8aab1
“Mason <mgentry@masongentry.com>
2024-01-04 10:29:00 -0800

    added ana audit

--- pages/audits/_audit.vue
+++ pages/audits/_audit.vue
@@ -2,9 +2,32 @@
   v-row.audit(justify="center" align="center" v-if="audit")
     v-col.layoutPadding.d-flex.justify-center.align-center(cols="12")
       Top(:text="audit.fullDescription")
+
+    
+    // Section for the hero images with headings
+    v-row
+      v-col(cols="6")
+         div.left-hero-wrapper.position-relative
+           img.left-hero(:src="getImagePath(audit.layout[0].media[0].name)" alt="audit.layout[0].media[0].title")
+           div.overlay-heading Before
+      v-col(cols="6")
+         div.right-hero-wrapper.position-relative
+           img.right-hero(:src="getImagePath(audit.layout[0].media[1].name)" alt="audit.layout[0].media[1].title")
+           div.overlay-heading After
+
+
+
+    // New section for title and text
+    v-col(cols="12" v-if="audit.layout && audit.layout[1].title")
+      .centered-section
+        h2 {{ audit.layout[1].title }}
+        div(v-for="paragraph in audit.layout[1].text" :key="paragraph")
+          p {{ paragraph }}
+
     v-col(cols="12")
       .points
         AuditImg(:imgSrc="auditImage" :points="audit.points")
+
     v-col.layoutPadding(cols="12")
       div.line
     v-col#formBlue(cols="12")
@@ -19,12 +42,20 @@
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex'
+import { mapState, mapActions } from 'vuex';
 import Top from "@/components/project/Top.vue";
+import AuditImg from "@/components/AuditImg.vue"; // Assuming you have this component
+import MailchimpFormBlue from "@/components/MailchimpFormBlue.vue"; // Assuming you have this component
+import Footer from "@/components/Footer.vue"; // Assuming you have this component
 
 export default {
   name: 'auditDetail',
-  components: {Top},
+  components: {
+    Top,
+    AuditImg,
+    MailchimpFormBlue,
+    Footer
+  },
   data() {
     return {
       audit: null,
@@ -49,6 +80,9 @@ export default {
       }
       this.audit = audit
     },
+    getImagePath(imageName) {
+      return `/domains/agency/audits/${this.audit.name}/media/${imageName}`;
+    },
   },
   async mounted() {
     this.$store.commit('updateState', {field: 'paddingLayout', value: false})
@@ -65,7 +99,21 @@ export default {
 }
 </script>
 
+
 <style lang="sass" scoped>
+
+.position-relative
+  position: relative
+
+.overlay-heading
+  position: absolute
+  top: -50px // Moves the text 50px higher than the top of the image
+  left: 50%
+  transform: translateX(-50%)
+  color: #FFF // Adjust the color as needed
+  font-size: 24px // Adjust the font size as needed
+  // Add more styling for the heading as needed
+
 .audit
   color: #FFFFFF
   padding: 0
@@ -73,6 +121,46 @@ export default {
   padding-bottom: 100px
 .container
   max-width: 650px
+
+.centered-section
+  text-align: left
+  max-width: 650px
+  margin: 0 auto
+
+h2
+ text-align: left
+
+p
+ text-align: left
+
+.left-hero-wrapper, .right-hero-wrapper
+  margin-left: 15px // For left image wrapper
+  margin-right: 15px // For right image wrapper
+
+.left-hero, .right-hero
+  width: 100%
+  height: auto
+  display: block
+
+.images
+  display: flex
+  align-items: center
+  justify-content: center
+
+img
+    width: 100%
+
+.left-hero, .right-hero
+  width: 100%
+  height: auto
+  display: block
+
+.audit-image
+  max-width: 100%
+  height: auto
+  display: block
+  margin: 0 auto
+  padding: 3px // Adjust this value as needed
 
 #bottomHeader
   font-size: 34px
