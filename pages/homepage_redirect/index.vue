4a48ec93371fec2b0067440a7f024cf10987ef01
mtgentry <mgentry@masongentry.com>
2025-01-13 10:01:34 -0800

    moved redirect folder location

---
+++ pages/homepage_redirect/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <!-- Empty div as we're just handling redirects -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomepageRedirect',
+  mounted() {
+    // Only run on client-side
+    if (process.client) {
+      const now = new Date().getTime();
+      const twoHoursInMillis = 2 * 60 * 60 * 1000;
+      const lastSessionTimestamp = localStorage.getItem("lastSessionTimestamp");
+
+      if (!lastSessionTimestamp || now - lastSessionTimestamp > twoHoursInMillis) {
+        localStorage.setItem("lastSessionTimestamp", now);
+        window.location.href = "https://trello.com/b/apWjOXh0/personal";
+      } else {
+        window.location.href = "https://www.google.com";
+      }
+    }
+  },
+  head() {
+    return {
+      title: 'Custom Start Page'
+    }
+  }
+}
+</script>
